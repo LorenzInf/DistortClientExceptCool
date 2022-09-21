3a7cf250952fd2d44e1d93deaf293308a254f354
LorenzInf <balzlore@kkg-do.eu>
2022-09-21 10:54:40 +0200

    Actual Vigenère this time

--- src/main/java/my_project/control/Client.java
+++ src/main/java/my_project/control/Client.java
@@ -4,7 +4,7 @@ import my_project.view.GUI;
 
 public class Client extends KAGO_framework.model.abitur.netz.Client {
 
-    private static final char[] keyword = "SKATIN".toCharArray();
+    private static final char[] keyword = "SKATIN".toCharArray(); //Has to be in capslock
     private String name;
     private GUI gui;
 
@@ -20,14 +20,14 @@ public class Client extends KAGO_framework.model.abitur.netz.Client {
             switch(args[0]){
                 case "MESSAGE" -> {
                     if (args.length > 2)
-                        gui.showText( args[1] + ": " + decode(args[2]));
+                        gui.showText( args[1] + ": " + code(args[2],false));
                 }
                 case "DM" -> {
                     if(args.length > 3) {
                         if(args[1].equals("RECEIVED")) {
-                            gui.showText(args[2] + " --> You: " + decode(args[3]));
+                            gui.showText(args[2] + " --> You: " + code(args[3],false));
                         } else if(args[1].equals("SENT")) {
-                            gui.showText("You --> " + args[2] + ": " + decode(args[3]));
+                            gui.showText("You --> " + args[2] + ": " + code(args[3],false));
                         }
                     }
                 }
@@ -69,10 +69,10 @@ public class Client extends KAGO_framework.model.abitur.netz.Client {
                     StringBuilder sb = new StringBuilder();
                     for (int i = 2; i < m.length; i++)
                         sb.append(m[i]).append(" ");
-                    send("WHISPER_" + m[1] + "_" + encode(sb.toString()));
+                    send("WHISPER_" + m[1] + "_" + code(sb.toString(),true));
                 }
             }
-            default -> send("MESSAGE_" + encode(message));
+            default -> send("MESSAGE_" + code(message,true));
         }
     }
 
@@ -80,6 +80,52 @@ public class Client extends KAGO_framework.model.abitur.netz.Client {
         this.gui=gui;
     }
 
+    //Normal
+    private String code(String message, boolean encode) {
+        if(message != null && !message.equals("")) { //Nur encoden, wenn die Nachricht ungleich null oder nicht leer ist
+
+            StringBuilder result = new StringBuilder(); //StringBuilder für das Ergebnis machen
+            var charMessage = message.toCharArray(); //Die Nachricht zu einem char Array machen
+
+            int j = 0;
+            for (char c : charMessage) { //Für alle chars in der Nachricht
+                if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) { //Wenn der inspizierte char zwischen a - z / A - Z ist
+                    if (j < keyword.length) //Jedes Mal, wenn wir am Ende des Schlüsselworts angekommen sind, wieder von vorne Anfangen
+                        j = 0;
+
+                    boolean isUppercase = Character.isUpperCase(c);
+                    // 65 abziehen, wenn er upperCase ist, 97 abziehen, wenn er lowercase ist. So ist das eben in ASCII.
+                    // Danach dann das Schlüsselwort -65 auf den Wert des Buchstaben addieren/subtrahieren, weil das immer uppercase ist
+                    int toAppend;
+                    if(encode)
+                        toAppend = ((int) c - (isUppercase ? 65 : 97)) + ((int) keyword[j] - 65);
+                    else
+                        toAppend = ((int) c - (isUppercase ? 65 : 97)) - ((int) keyword[j] - 65);
+
+                    if(encode) {
+                        if (toAppend > 25) // Wenn das Ergebnis größer als 25 ist (und wir encoden)
+                            toAppend -= 25; // Loopen wir wieder rum
+                    } else {
+                        if (toAppend < 0) // Wenn das Ergebnis kleiner als 25 ist (und wir decoden)
+                            toAppend += 25; // Loopen wir wieder rum
+                    }
+
+                    toAppend += isUppercase ? 65 : 97; // Dann wieder den richtigen Wert addieren
+
+                    result.append((char) toAppend); // Und appenden
+                    j++;
+                } else {
+                    result.append(c); // Wenn es kein Buchstabe im Alphabet war einfach appenden
+                }
+            }
+            return result.toString();
+        }
+        return null;
+    }
+
+    // Die beiden Methoden sind sehr ähnlich, also gibt es bestimmt einen kürzeren Weg, den haben wir jetzt aber nicht
+
+    /* ASCII
     private String encode(String message) {
         if(message != null && !message.equals("")) {
             StringBuilder result = new StringBuilder();
@@ -113,4 +159,5 @@ public class Client extends KAGO_framework.model.abitur.netz.Client {
         }
         return null;
     }
+    */
 }
